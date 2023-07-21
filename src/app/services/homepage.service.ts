ddc237a6241694934187356688018e79fdcb6e4f
Kyubs1610 <diego.decort@gmail.com>
2023-07-21 17:08:52 +0200

    production: true

--- src/app/services/homepage.service.ts
+++ src/app/services/homepage.service.ts
@@ -1,7 +1,7 @@
-import { environment } from './../../environment';
+import { environment } from './../environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
